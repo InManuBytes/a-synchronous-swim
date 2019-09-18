4339822431d6a419abbac56095e918ebea926b8b
RomanEmmons <roman.emmons@gmail.com>
2019-09-18 00:52:01 -0700

    Get response from postman

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -15,47 +15,53 @@ module.exports.initialize = queue => {
 
 module.exports.router = (req, res, next = () => {}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
-  res.writeHead(200, headers);
-  res.end();
-  next(); // invoke next() at the end of a request to help with testing!
+  if (req.method === 'GET') {
+    res.writeHead(200, headers);
+    console.log('RES', res);
+    res.end();
+    next();
+  }
+  // res.writeHead(200, headers);
+  // res.end();
+  // next(); // invoke next() at the end of a request to help with testing!
 };
 
 // everything from here down was added by us...
-http
-  .get('http://127.0.0.1:3000', res => {
-    const { statusCode } = res;
-    const contentType = res.headers['content-type'];
+// http
+//   .get('http://127.0.0.1:3000', res => {
+//     const { statusCode } = res;
+//     const contentType = res.headers['content-type'];
 
-    let error;
-    if (statusCode !== 200) {
-      error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
-    } else if (!/^application\/json/.test(contentType)) {
-      error = new Error(
-        'Invalid content-type.\n' +
-          `Expected application/json but received ${contentType}`
-      );
-    }
-    if (error) {
-      console.error(error.message);
-      // consume response data to free up memory
-      res.resume();
-      return;
-    }
+//     let error;
+//     if (statusCode !== 200) {
+//       error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+//     } else if (!/^application\/json/.test(contentType)) {
+//       error = new Error(
+//         'Invalid content-type.\n' +
+//           `Expected application/json but received ${contentType}`
+//       );
+//     }
+//     if (error) {
+//       console.error(error.message);
+//       // consume response data to free up memory
+//       res.resume();
+//       return;
+//     }
 
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', chunk => {
-      rawData += chunk;
-    });
-    res.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        console.log(parsedData);
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
-  })
-  .on('error', e => {
-    console.error(`Got error: ${e.message}`);
-  });
+//     res.setEncoding('utf8');
+//     let rawData = '';
+//     res.on('data', chunk => {
+//       rawData += chunk;
+//     });
+//     res.on('end', () => {
+//       try {
+//         const parsedData = JSON.parse(rawData);
+//         console.log(parsedData);
+//       } catch (e) {
+//         console.error(e.message);
+//       }
+//     });
+//   })
+//   .on('error', e => {
+//     console.error(`Got error: ${e.message}`);
+//   });
