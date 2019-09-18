cd1fd16c9b055b53328213076ce8706b980d25b1
RomanEmmons <roman.emmons@gmail.com>
2019-09-18 00:25:24 -0700

    Progress with implementing swim command

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -2,19 +2,60 @@ const fs = require('fs');
 const path = require('path');
 const headers = require('./cors');
 const multipart = require('./multipartUtils');
+const http = require('http');
 
 // Path for the background image ///////////////////////
 module.exports.backgroundImageFile = path.join('.', 'background.jpg');
 ////////////////////////////////////////////////////////
 
 let messageQueue = null;
-module.exports.initialize = (queue) => {
+module.exports.initialize = queue => {
   messageQueue = queue;
 };
 
-module.exports.router = (req, res, next = ()=>{}) => {
+module.exports.router = (req, res, next = () => {}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
   res.end();
   next(); // invoke next() at the end of a request to help with testing!
 };
+
+// everything from here down was added by us...
+http
+  .get('http://127.0.0.1:3000', res => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
+
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+      error = new Error(
+        'Invalid content-type.\n' +
+          `Expected application/json but received ${contentType}`
+      );
+    }
+    if (error) {
+      console.error(error.message);
+      // consume response data to free up memory
+      res.resume();
+      return;
+    }
+
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', chunk => {
+      rawData += chunk;
+    });
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        console.log(parsedData);
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  })
+  .on('error', e => {
+    console.error(`Got error: ${e.message}`);
+  });
--- server/spec/httpHandler.spec.js
+++ server/spec/httpHandler.spec.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs');
 const path = require('path');
 const expect = require('chai').expect;
@@ -6,12 +5,9 @@ const server = require('./mockServer');
 
 const httpHandler = require('../js/httpHandler');
 
-
-
 describe('server responses', () => {
-
-  it('should respond to a OPTIONS request', (done) => {
-    let {req, res} = server.mock('/', 'OPTIONS');
+  it('should respond to a OPTIONS request', done => {
+    let { req, res } = server.mock('/', 'OPTIONS');
 
     httpHandler.router(req, res);
     expect(res._responseCode).to.equal(200);
@@ -21,14 +17,18 @@ describe('server responses', () => {
     done();
   });
 
-  it('should respond to a GET request for a swim command', (done) => {
+  it('should respond to a GET request for a swim command', done => {
     // write your test here
+    let { req, res } = server.mock('/', 'GET');
+    httpHandler.router(req, res);
+    expect(res._responseCode).to.equal(200);
+    expect(res._data.toString()).to.equal('up');
     done();
   });
 
-  xit('should respond with 404 to a GET request for a missing background image', (done) => {
+  xit('should respond with 404 to a GET request for a missing background image', done => {
     httpHandler.backgroundImageFile = path.join('.', 'spec', 'missing.jpg');
-    let {req, res} = server.mock('FILL_ME_IN', 'GET');
+    let { req, res } = server.mock('FILL_ME_IN', 'GET');
 
     httpHandler.router(req, res, () => {
       expect(res._responseCode).to.equal(404);
@@ -37,17 +37,17 @@ describe('server responses', () => {
     });
   });
 
-  xit('should respond with 200 to a GET request for a present background image', (done) => {
+  xit('should respond with 200 to a GET request for a present background image', done => {
     // write your test here
     done();
   });
 
   var postTestFile = path.join('.', 'spec', 'water-lg.jpg');
 
-  xit('should respond to a POST request to save a background image', (done) => {
+  xit('should respond to a POST request to save a background image', done => {
     fs.readFile(postTestFile, (err, fileData) => {
       httpHandler.backgroundImageFile = path.join('.', 'spec', 'temp.jpg');
-      let {req, res} = server.mock('FILL_ME_IN', 'POST', fileData);
+      let { req, res } = server.mock('FILL_ME_IN', 'POST', fileData);
 
       httpHandler.router(req, res, () => {
         expect(res._responseCode).to.equal(201);
@@ -57,7 +57,7 @@ describe('server responses', () => {
     });
   });
 
-  xit('should send back the previously saved image', (done) => {
+  xit('should send back the previously saved image', done => {
     fs.readFile(postTestFile, (err, fileData) => {
       httpHandler.backgroundImageFile = path.join('.', 'spec', 'temp.jpg');
       let post = server.mock('FILL_ME_IN', 'POST', fileData);
